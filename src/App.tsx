232c866b95c4683c78ff0f07da790d79ec746997
evanh237 <evanh237@gmail.com>
2024-07-29 16:17:18 -0500

    finished win/loss logic, added button to play again

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
-// import "./App.css";
-import { useCallback, useEffect, useState } from "react";
+import { useCallback, useEffect, useState, CSSProperties } from "react";
 import words from "./wordList.json";
 import { HangmanDrawing } from "./hangmanDrawing";
 import { HangmanWord } from "./hangmanWord";
 import { Keyboard } from "./keyboard";
 
+function getWord() {
+  return words[Math.floor(Math.random() * words.length)];
+}
+
 function App() {
-  const [wordToGuess, setWordToGuess] = useState(() => {
-    return words[Math.floor(Math.random() * words.length)];
-  });
+  const [wordToGuess, setWordToGuess] = useState(getWord());
 
   const [guessedLetters, setGuessedLetters] = useState<string[]>([]);
 
@@ -16,13 +17,18 @@ function App() {
     (letter) => !wordToGuess.includes(letter)
   );
 
+  const isLoser = incorrectLetters.length >= 6;
+  const isWinner = wordToGuess
+    .split("")
+    .every((letter) => guessedLetters.includes(letter));
+
   const addGuessedLetter = useCallback(
     (letter: string) => {
-      if (guessedLetters.includes(letter)) return;
+      if (guessedLetters.includes(letter) || isLoser || isWinner) return;
 
       setGuessedLetters((currentLetters) => [...currentLetters, letter]);
     },
-    [guessedLetters]
+    [guessedLetters, isLoser, isWinner]
   );
 
   useEffect(() => {
@@ -40,6 +46,38 @@ function App() {
     };
   }, [guessedLetters]);
 
+  useEffect(() => {
+    const handler = (e: KeyboardEvent) => {
+      const key = e.key;
+      if (key !== "Enter") return;
+
+      e.preventDefault();
+      setGuessedLetters([]);
+      setWordToGuess(getWord());
+    };
+    document.addEventListener("keypress", handler);
+
+    return () => {
+      document.removeEventListener("keypress", handler);
+    };
+  }, [guessedLetters]);
+
+  const newGame = () => {
+    window.location.reload();
+  };
+
+  const buttonStyle: CSSProperties = {
+    fontSize: "1rem",
+    padding: "0.5rem 1rem",
+    margin: "1rem 0",
+    cursor: "pointer",
+    border: "none",
+    borderRadius: "5px",
+    backgroundColor: "#4caf50",
+    color: "white",
+    textAlign: "center",
+  };
+
   return (
     <div
       style={{
@@ -51,11 +89,34 @@ function App() {
         alignItems: "center",
       }}
     >
-      <div style={{ fontSize: "2rem", textAlign: "center" }}> Lose Win</div>
+      <div style={{ fontSize: "2rem", textAlign: "center" }}>
+        {" "}
+        {isWinner && (
+          <>
+            Winner! {""}
+            <button onClick={newGame} style={buttonStyle}>
+              Play Again?
+            </button>
+          </>
+        )}
+        {isLoser && (
+          <>
+            Dang! Didn't Guess the Word! {""}
+            <button onClick={newGame} style={buttonStyle}>
+              Play Again?
+            </button>
+          </>
+        )}
+      </div>
       <HangmanDrawing numberOfGuesses={incorrectLetters.length} />
-      <HangmanWord guessedLetters={guessedLetters} wordToGuess={wordToGuess} />
+      <HangmanWord
+        reveal={isLoser}
+        guessedLetters={guessedLetters}
+        wordToGuess={wordToGuess}
+      />
       <div style={{ alignSelf: "stretch" }}>
         <Keyboard
+          disabled={isWinner || isLoser}
           activeLetter={guessedLetters.filter((letter) =>
             wordToGuess.includes(letter)
           )}
